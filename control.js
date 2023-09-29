3f6fcd0413e020550b4859bc724c9ff9d0ad2573
julia de lima <lima.machado@alunoescolassociais.org.br>
2023-09-29 10:37:46 -0300

    construção das pasginas

---
+++ control.js
@@ -0,0 +1,55 @@
+'use restrict'; //Modo restrito
+
+ 
+
+//Limpar formulário
+
+const limparFormulario = () =>{
+
+    document.getElementById('rua').value = '';
+
+    document.getElementById('bairro').value = '';
+
+    document.getElementById('cidade').value = '';
+
+    document.getElementById('estado').value = '';
+
+}
+
+ 
+
+//Verifica se CEP é válido
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+ 
+
+//Preenche campos do formulário
+
+const preencherFormulario = (endereco) =>{
+
+    document.getElementById('rua').value = endereco.logradouro;
+
+    document.getElementById('bairro').value = endereco.bairro;
+
+    document.getElementById('cidade').value = endereco.localidade;
+
+    document.getElementById('estado').value = endereco.uf;
+
+}
+
+ 
+
+/*
+
+Função para consumo de API
+
+utilizando a função do tipo assíncrona
+
+*/
+
+const pesquisarcep = async() =>{
+
+    limparFormulario();
