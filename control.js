36c4279615ebbdb16fe631e65962967f09c38716
julia de lima <lima.machado@alunoescolassociais.org.br>
2023-10-06 09:52:09 -0300

    aterando codigos errado para o certo

--- control.js
+++ control.js
@@ -1,24 +1,24 @@
-'use restrict'; //Modo restrito
+'use restrict'; //modo restrito
 
  
 
-//Limpar formulário
+//limpar formulario
 
 const limparFormulario = () =>{
 
     document.getElementById('rua').value = '';
 
-    document.getElementById('bairro').value = '';
+    document.getElementById('estado').value = '';
 
     document.getElementById('cidade').value = '';
 
-    document.getElementById('estado').value = '';
+    document.getElementById('bairro').value = '';
 
 }
 
  
 
-//Verifica se CEP é válido
+// verifica se o cep é valido
 
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 
@@ -26,17 +26,19 @@ const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
  
 
-//Preenche campos do formulário
+//preenche campos do formulario
 
 const preencherFormulario = (endereco) =>{
 
     document.getElementById('rua').value = endereco.logradouro;
 
-    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('estado').value = endereco.bairro;
 
     document.getElementById('cidade').value = endereco.localidade;
 
-    document.getElementById('estado').value = endereco.uf;
+    document.getElementById('bairro').value = endereco.uf;
+
+ 
 
 }
 
@@ -44,12 +46,50 @@ const preencherFormulario = (endereco) =>{
 
 /*
 
-Função para consumo de API
-
-utilizando a função do tipo assíncrona
+função para consumo de API ultilizando a função do tipo assincrona
 
 */
 
-const pesquisarcep = async() =>{
+ 
+
+const pesquisarCep = async() =>{
 
     limparFormulario();
+
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+
+   
+
+    if(cepValido(cep.value)){
+
+        const dados = await fetch(url);
+
+        const addres = await dados.json();
+
+ 
+
+        if(addres.hasOwnProperty('erro')){
+
+            alert('CEP não encontrado');
+
+        }else{
+
+            preencherFormulario(addres);
+
+        }
+
+    }else{
+
+        alert('CEP incorreto');
+
+    }
+
+}
+
+ 
+
+//add um comentario DOM, no input CEP
+
+ 
+
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
